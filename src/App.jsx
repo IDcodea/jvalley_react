73c61d35b76aee99a682bee4fdad932b902dcd1c
IDCodea <arismunandar1194@gmail.com>
2023-03-02 17:19:29 +0700

    stage one

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar"
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    </>
+  )
+}
